92a7a45d04ab2c24c23b91ba33538b29eb41423f
jqzhang <jqzhang@h-visions.cn>
2026-05-04 20:10:22 +0800

    修改

--- Jenkinsfile
+++
@@ -1,3 +0,0 @@
-@Library('first-shared-lib') _
-//welcomeJob 'lambdatest'
-jenkinsSample 'https://github.com/iamvickyav/spring-boot-data-H2-embedded.git'
--- src/test/java/com/iamvickyav/springboot/SpringBootRestWithH2/EmployeeControllerTest.java
+++ src/test/java/com/iamvickyav/springboot/SpringBootRestWithH2/EmployeeControllerTest.java
@@ -1,29 +1,22 @@
 package com.iamvickyav.springboot.SpringBootRestWithH2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import com.iamvickyav.springboot.SpringBootRestWithH2.model.Employee;
 import com.iamvickyav.springboot.SpringBootRestWithH2.service.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
